f7bced4f7525192ccfc8bbac00bcbd503dc6b2f9
thesloth98 <aleksandrabalkanska@gmail.com>
2023-05-31 13:32:49 +0900

    Update Env Variables

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ app.config['SECRET_KEY'] = secret_key
 Bootstrap5(app)
 
 projects = requests.get("https://api.npoint.io/f4984877ce0243630b82").json()
-
 project_objects = []
 for project in projects:
     project_obj = Project(project["title"], project["link"], project['description'], project["image_path"],
